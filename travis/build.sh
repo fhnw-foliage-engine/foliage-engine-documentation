3f30225917a754785574b60d4b3d1c367f0c7e28
Livio Bieri <livio@livio.li>
2015-10-19 10:00:57 +0200

    sudo: required

--- travis/build.sh
+++ travis/build.sh
@@ -4,7 +4,7 @@ echo '#                Build               #'
 echo '#              - START -             #'
 echo '######################################'
 
-PATH=$PATH:/tmp/texlive2015/x86_64-linux/
+## PATH=$PATH:/tmp/texlive2015/x86_64-linux/
 make all
 
 echo '######################################'
